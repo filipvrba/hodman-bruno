a29ec73570894ee100fd29102f05fc48f60a4a54
Filip Vrba <filipvrba@pm.me>
2024-05-31 18:38:41 +0200

    add button for services card

--- src/js/elements/elm_services.js
+++ src/js/elements/elm_services.js
@@ -22,11 +22,16 @@ export default class ElmServices extends HTMLElement {
       for (let service of servicesObj.services) {
         let serviceTemplate = `${`
 <div class='col-md-6 mb-4'>
-  <div class='card'>
+  <div class='card h-100'>
     <elm-lazy-image src='${service.picture}' class='card-img-top' alt='${service.name}'></elm-lazy-image>
     <div class='card-body'>
       <h5 class='card-title'>${service.name}</h5>
       <p class='card-text'>${service.description}</p>
+    </div>
+    <div class='mt-auto'>
+      <div class='text-center mb-3'>
+        <a href='#kontakt' class='btn btn-primary'>Kontaktujte nás</a>
+      </div>
     </div>
   </div>
 </div>
